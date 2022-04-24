285d8da2d0e135fda3132189c6f8ec0e1304896b
jami-it <abdulrehmanjami73@gmail.com>
2022-04-24 21:49:58 +0500

    Data_Type

---
+++ JavaScript_Data_Types.js
@@ -0,0 +1,16 @@
+// JavaScript Data Types
+// // avaScript variables can hold different data types: numbers, strings, objects and more:
+// JavaScript variables can hold different data types: numbers, strings, objects and more:
+let length=145;
+let last_name="Jami";
+let x={firstName:"Abdul Rehman", lastName:"Jami"};
+console.log(x);
+
+// The Concept of Data Types
+// In programming, data types is an important concept.
+// To be able to operate on variables, it is important to know something about the type.
+// Without data types, a computer cannot safely solve this:
+x="Jami" + 21;
+console.log(x);
+x=16+5 +"  " +"Jami";
+
