e3566452ed1707012f301f46101a2a964f7811f6
jami-it <abdulrehmanjami73@gmail.com>
2022-04-24 20:33:11 +0500

    varidablae

---
+++ var_const.js
@@ -0,0 +1,13 @@
+
+// The const keyword was introduced in ES6 (2015).
+
+// Variables defined with const cannot be Redeclared.
+
+// Variables defined with const cannot be Reassigned.
+
+// Variables defined with const have Block Scope.
+
+const po=1.21;
+//PI=3
+console.log(po);
+
