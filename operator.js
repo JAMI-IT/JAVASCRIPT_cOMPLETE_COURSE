eded0558dbbc748b8c43031b1dc606e851f66af1
jami-it <abdulrehmanjami73@gmail.com>
2022-04-24 20:51:44 +0500

    operators

---
+++ operator.js
@@ -0,0 +1,21 @@
+//Operator
+
+//Divide operator (/)..........
+let x=5;
+let y=10;
+let z;
+z=y/x;
+console.log(z);
+
+//Mulutiplication OPerator(*)..
+z=x*y;
+console.log(z);
+
+//Addition Operator (+).........
+z=x+y;
+console.log(z);
+
+//Substraction Operator(-).......
+z=y-x;
+console.log(z);
+
