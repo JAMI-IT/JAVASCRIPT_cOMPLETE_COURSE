67eae5caec6041d02c19e0905d6001a83c7c4300
jami-it <abdulrehmanjami73@gmail.com>
2022-04-24 20:34:51 +0500

    varcosnt

--- var_let.js
+++ var_let.js
@@ -1,2 +1,14 @@
+
+
+// The let keyword was introduced in ES6 (2015).
+
+// Variables defined with let cannot be Redeclared.
+
+// Variables defined with let must be Declared before use.
+
+// Variables defined with let have Block Scope.
+
+
+
 let Car_name ='BMW'
 console.log(Car_name);
