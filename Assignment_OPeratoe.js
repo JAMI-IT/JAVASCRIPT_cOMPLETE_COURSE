50e97486cabf0f4ae5c83be8f015c9c62bc7d344
jami-it <abdulrehmanjami73@gmail.com>
2022-04-24 21:49:39 +0500

    Assignment_Operator

---
+++ Assignment_OPeratoe.js
@@ -0,0 +1,26 @@
+// JavaScript Assignment Operators
+// Assignment operators assign values to JavaScript variables.
+
+// The = assignment operator assigns a value to a variable.
+let x=10;
+
+// The += assignment operator adds a value to a variable.
+x +=5;
+console.log(x);
+
+// The -= assignment operator subtracts a value from a variable.
+x -=5;
+console.log(x);
+
+// The *= assignment operator multiplies a variable.
+x *=2;
+console.log(x);
+
+// The /= assignment divides a variable.
+x /=2;
+console.log(x);
+
+// The %= assignment operator assigns a remainder to a variable.
+x %=2;
+console.log(x);
+
