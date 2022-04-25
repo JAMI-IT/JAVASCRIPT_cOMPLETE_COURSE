443c4716c38391bfdd685a4967967067af4d8286
jami-it <abdulrehmanjami73@gmail.com>
2022-04-25 22:30:31 +0500

    objects

---
+++ objects.js
@@ -0,0 +1,12 @@
+// Objects are variables too. But objects can contain many values.
+// This code assigns many values (Fiat, 500, white) to a variable named car:
+const car ={ Name:"BMW", Color:"Blue",price:"5000"};
+
+console.log("Car Name="+car.Name,"Caar Color ="+car.Color,"Car Price"+car.price);
+
+const person ={
+    Name:"Jami",
+    Age:"22",
+    Adrees:"LORAN"
+};
+console.log("Person NAme ="+person.Name,"Person AGE ="+person.Age,"Person Address ="+person.Adrees);
