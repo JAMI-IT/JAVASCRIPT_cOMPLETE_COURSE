74e5a907e3a37964b0b5c5b28f5b7f9e25c09062
jami-it <abdulrehmanjami73@gmail.com>
2022-04-26 21:48:26 +0500

    String

---
+++ string.js
@@ -0,0 +1,36 @@
+// String Length
+
+// To find the length of a string, use the built-in length property:
+
+let text="Abdul rehman Jammi";
+
+console.log(text.length);
+
+// The sequence \"  inserts a double quote in a string:
+let text1= "Iam Student Of \"IT\" From UOG";
+console.log(text1);
+
+// The sequence \'  inserts a single quote in a string:
+let text2="It\'s my Personal";
+console.log(text2);
+
+// The sequence \\  inserts a backslash in a string:
+let text3="to inert \\ backslash ";
+console.log(text3);    
+
+// Code 	Result
+// \b 	Backspace
+// \f 	Form Feed
+// \n 	New Line
+// \r 	Carriage Return
+// \t 	Horizontal Tabulator
+// \v 	Vertical Tabulator
+
+// JavaScript Strings as Objects
+
+// Normally, JavaScript strings are primitive values, created from literals:
+let x= "Jami";
+let y=new String("Jami");
+x==y;
+console.log(x);
+
