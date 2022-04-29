f5535c71452d765111f9ac59f36109d94ac61b3d
jami-it <abdulrehmanjami73@gmail.com>
2022-04-29 21:32:49 +0500

    String_Method

--- String_Method.js
+++ String_Method.js
@@ -1,7 +1,5 @@
 // Extracting String Parts
-
 // There are 3 methods for extracting a part of a string:
-
 //     slice(start, end)
 //     substring(start, end)
 //     substr(start, length)
@@ -30,47 +28,38 @@ console.log(text.replace(/MARGHZAR/i,"UOG"));
 console.log(text.replace(/UOG/g,"MARGHZAR"));
 
 // Converting to Upper and Lower Case
-
 // A string is converted to upper case with toUpperCase():
-
 // A string is converted to lower case with toLowerCase():
 console.log(text.toUpperCase());
 console.log(text.toLowerCase());
 
 // JavaScript String concat()
-
 // concat() joins two or more strings:
 console.log(text.concat("   ",text2));
 
 // JavaScript String trim()
-
 // The trim() method removes whitespace from both sides of a string:
 // Example
 let a="                 My Name IS JAMI ";
 console.log(a.trim());
 
 // JavaScript String padStart()
-
 // The padStart() method pads a string with another string:
 
 console.log(a.padStart(14,"0"));
 
 // JavaScript String padEnd()
-
 // The padEnd() method pads a string with another string:
 let x=4;
 let to=x.toString();
 console.log(to.padEnd(4,"0"));
 
 // JavaScript String charAt()
-
 // The charAt() method returns the character at a specified index (position) in a string:
 console.log(text.charAt(0));
 
 // JavaScript String charCodeAt()
-
 // The charCodeAt() method returns the unicode of the character at a specified index in a string:
-
 // The method returns a UTF-16 code (an integer between 0 and 65535).
 console.log(text.charCodeAt(0));
 
