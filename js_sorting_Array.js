71a5378345f0afa74b49e6f769a34ef77e6757d0
jami-it <abdulrehmanjami73@gmail.com>
2022-05-10 00:08:34 +0500

    JS_ARRAY_MEthod_1

---
+++ js_sorting_Array.js
@@ -0,0 +1,9 @@
+// Sorting an Array
+// The sort() method sorts an array alphabetically: 
+
+let no=[11,21,23,65,21,11];
+let people=["Abdul Rehmnan","Jami","Shahjahan","Humayu"];
+console.log(people.sort().toString());
+console.log(people.reverse().toString());
+console.log(people.join(" * "));
+console.log(no.sort());
