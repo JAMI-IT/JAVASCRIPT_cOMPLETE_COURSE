6837fa0f5baa8d09966ab44e5a1ecdea584ba82c
jami-it <abdulrehmanjami73@gmail.com>
2022-05-11 12:36:57 +0500

    JS_sorting_array

--- JS_Array_foreach.js
+++ JS_Array_foreach.js
@@ -22,7 +22,7 @@ function myfunction(value,index,array)
 // The map() method does not execute the function for array elements without values.
 // The map() method does not change the original array.
 // This example multiplies each array value by 2: 
-
+const str=["Jami","Shahjahan","Humayu","Gujjar"];
 const no1=[2,5,3,8,7];
 const no2=no1.map(myfunc);
 console.log("Teh multiplee of 2");
@@ -55,11 +55,57 @@ function reducer(total,value,index,array) {
     return total*value;
 }
 
-const no6=no1.every(over);
 
 console.log("The every no is over to 2");
+
+const no6=no1.every(over);
 console.log(no6);
 function over(value,index,array) {
-    return value>1;
+    if (value>1) {
+        console.log("The all vaue is above then 2 :");
+    }
+    else
+    {
+        console.log("The all valuue is no greter then 2");
+    }
+
 }
 
+
+// JavaScript Array some()
+// The some() method check if some array values pass a test.
+// This example check if some array values are larger than 18:
+
+const no7=no1.some(soom);
+console.log("THE USE OF SOME COMD");
+console.log(no7);
+
+function soom(value,index,array) {
+
+    return value>2;
+
+}
+
+let s=str.indexOf("Humayu")+1;
+console.log(s+" :  THe index Of humyau");
+
+s=str.lastIndexOf("Humayu")+1;
+console.log(s+":  The LAST INDEX OF humyu");
+
+let no8=no1.find(fin);
+console.log(no8);
+function fin(value,index,aray) {
+    return value>7;
+}
+
+no8=no1.findIndex(FIND);
+console.log(no8+1);
+function FIND(value,index,array) {
+    return value>2;
+}
+
+const f=str.entries();
+for (let index = 0; index < array.length; index++) {
+    const element = str[index];
+    console.log(element);
+}
