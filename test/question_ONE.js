0a91d4f2f3c4fadab45d2db1de7ce72247671b23
jami-it <abdulrehmanjami73@gmail.com>
2022-05-13 12:43:21 +0500

    Question_ONE

---
+++ test/question_ONE.js
@@ -0,0 +1,14 @@
+const a={
+    add : [
+         {name:"ali",age:200},
+         {name:"shah",age:2031},
+         {name:"arslan",age:2038},
+         {name:"ajmi",age:2033},
+         {name:"shah",age:2034}
+     ]
+     };
+
+      // a)Identify the piece of code is it array or object
+     console.log("a)Identify the piece of code is it array or object");
+    let t=typeof a;
+    console.log(t);
