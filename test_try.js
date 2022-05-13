405a76e1572d8b373c63efd74566108a9fb9bf6c
jami-it <abdulrehmanjami73@gmail.com>
2022-05-13 12:50:17 +0500

    test_try

---
+++ test_try.js
@@ -0,0 +1,28 @@
+
+const a={
+    add : [
+         {name:"ali",age:200},
+         {name:"shah",age:2031},
+         {name:"arslan",age:2038},
+         {name:"ajmi",age:2033},
+         {name:"shah",age:2034}
+     ]
+     };
+var c;
+    for(var i of a.add){
+        // console.log(i);
+
+    }
+   var f= function add(){
+    for(var i=0;i<a.add.length;i++){
+      var m= Math.max(a.add[i].age)
+    
+    }
+    return m
+    }
+    var t=f()
+ console.log(t);
+    // console.log(f);
+
+    // console.log(a.add[i].age);
+    for (var j=0;j<a.add)
