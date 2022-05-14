dc01229d13f3bad292c109e52222656114c1ec01
jami-it <abdulrehmanjami73@gmail.com>
2022-05-14 11:18:17 +0500

    Question5

---
+++ test/quesion5.js
@@ -0,0 +1,17 @@
+const a={
+    add : [
+         {name:"ali",age:200},
+         {name:"shah",age:2031},
+         {name:"arslan",age:2038},
+         {name:"ajmi",age:2033},
+         {name:"shah",age:2034}
+     ]
+     };
+
+
+// e)write a object and push this object into this piece of code
+
+let p={name:"Humyu",age:20};
+
+a.add.push(p);
+console.log(a.add);
