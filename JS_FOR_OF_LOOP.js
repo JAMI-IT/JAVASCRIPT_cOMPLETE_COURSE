9b4f9531453622facbfe99784ce0e14ad387fa38
jami-it <abdulrehmanjami73@gmail.com>
2022-05-16 16:08:30 +0500

    for of loop

---
+++ JS_FOR_OF_LOOP.js
@@ -0,0 +1,18 @@
+
+let a=["BMW","BUGHATI","FERAYRY"];
+let text="";
+
+for(let x of a)
+{
+    text +=x+" _ ";
+}
+console.log(text);
+
+let l="Abdulrehmanjami"
+let text1="";
+for(let x of l)
+{
+    text1 +=x+" _ ";
+}
+console.log(text1);
+
