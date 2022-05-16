46df49978034df7449364fda3737f193656f4af3
jami-it <abdulrehmanjami73@gmail.com>
2022-05-16 16:07:51 +0500

    question 7

---
+++ test/question7.js
@@ -0,0 +1,14 @@
+
+const a={
+    add : [
+         {name:"ali",age:200},
+         {name:"shah",age:2031},
+         {name:"arslan",age:2038},
+         {name:"ajmi",age:2033},
+         {name:"shah",age:2034}
+     ]
+     };
+
+// //      g)write a code which removes the two idexes from the piece of code
+a.add.splice(1,2);
+console.log(a.add);
