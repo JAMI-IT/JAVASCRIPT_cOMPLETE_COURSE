08134a6f6f13a08f9c0b2e9e95f31a0455f4f13a
jami-it <abdulrehmanjami73@gmail.com>
2022-05-17 08:28:26 +0500

    math

---
+++ JS_MATH.js
@@ -0,0 +1,19 @@
+let a=Math.PI
+console.log(a);
+
+let m=[Math.E,Math.PI,Math.SQRT2,Math.SQRT1_2,Math.LN2,Math.LN10,Math.LOG2E,Math.LOG10E];
+
+let text=
+''
+for(let x in m)
+{
+    text +=m[x]+" _ ";
+
+}
+console.log(text);
+
+let mat=[12.2,12.7,32,];
+console.log(Math.round(mat[1]));
+
+console.log(Math.ceil(mat[0]));
+
