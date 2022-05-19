38e2d0f77d87eb3e70f04330fc7e52cf9741a83c
JAMI-IT <103955140+JAMI-IT@users.noreply.github.com>
2022-05-19 08:24:31 +0500

    JS_IF_ELSE_STATEMENT

---
+++ JD_IF_ELSE.js
@@ -0,0 +1,16 @@
+
+
+const h=23;
+if (h<22) {
+    console.log('The age is smaller then the 22 :'+h);
+}
+
+else if (h>22) {
+    console.log("THe age is greter hten 22 :"+h);
+}
+
+else 
+{
+    console.log('The age is greater then the 22 :'+h);
+}
+
