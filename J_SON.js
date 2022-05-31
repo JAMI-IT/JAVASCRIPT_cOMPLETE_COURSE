a86d3caece588b62c0b1bc1382e3af00bff7d280
jami-it <abdulrehmanjami73@gmail.com>
2022-05-31 21:34:58 +0500

    json

--- J_SON.js
+++ J_SON.js
@@ -8,9 +8,12 @@
 
 
 console.log(jsonobj);
+console.log("This can convert the array into string");
 let jsonstr=JSON.stringify(jsonobj)
 console.log(jsonstr);
 
 console.log(jsonstr.replace('Bole','GEO'));
+
 jsonobj=JSON.parse(jsonstr);
 console.log(jsonobj);
+
