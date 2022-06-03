3721aed46db299f1f34693fcf3102a39beec0d3b
jami-it <abdulrehmanjami73@gmail.com>
2022-06-03 11:06:36 +0500

    promise

--- JSAsync/jsAsync.js
+++ JSAsync/jsAsync.js
@@ -15,3 +15,27 @@ function myfun()
 }
 
 
+function disply(text)
+{
+  console.log(text);
+}
+let mypromis =new Promise(function(myresovle,myreject)
+{
+    let x=0;
+    if (x==0)
+    {
+      myresovle("OK");
+    }
+    else{
+      myreject("Error");
+   }
+   });
+
+    mypromis.then(
+      function(value){disply(value)},
+      function(error){disply(error)}
+    )
+
+
+
+
